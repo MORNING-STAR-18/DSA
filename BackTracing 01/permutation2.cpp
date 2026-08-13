ffb7610fe4dfd6c92706a454bc63eb32d0074409
MORNING-STAR-18 <sumitsingh76406@gmail.com>
2026-08-13 21:05:59 +0530

    Added new DSA folders and files

--- BackTracing 01/permutation2.cpp
+++ BackTracing 01/permutation2.cpp
@@ -1,17 +1,21 @@
 #include<iostream>
+#include<unordered_set>
 using namespace std;
 void per(string &str, int i){
     if(i == str.size() - 1){
         cout << str << endl;
         return;
     }
+    unordered_set<char> s;
     for(int idx = i; idx < str.size(); idx ++){
+        if(s.count(str[idx])) continue;
+        s.insert(str[idx]);
         swap(str[idx],str[i]);
         per(str, i + 1);
         swap(str[idx], str[i]);
     }
 }
 int main(){
-    string str = "abc";
+    string str = "aba";
     per(str, 0);
 }
